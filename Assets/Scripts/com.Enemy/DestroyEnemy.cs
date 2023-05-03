7fa0341006228ceb4498a6e2959f54436b073bd1
Eli <78963198+niceEli@users.noreply.github.com>
2023-05-03 10:00:55 -0400

    added a fireball and made you jump when goin on enemys

--- Assets/Scripts/com.Enemy/DestroyEnemy.cs
+++ Assets/Scripts/com.Enemy/DestroyEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DestroyEnemy : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class DestroyEnemy : MonoBehaviour
         // Check if the colliding object is on the player layer
         if (((1 << collision.gameObject.layer) & playerLayer) != 0)
         {
+            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+            RigidbodyMovement playerM = collision.GetComponent<RigidbodyMovement>();
+            playerRb.velocity = new Vector2(playerRb.velocity.x, playerM.jumpForce);
             // Destroy the object that this script is attached to
             Destroy(gotd);
         }
--- Assets/Scripts/com.Player/RigidbodyMovement.cs
+++ Assets/Scripts/com.Player/RigidbodyMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class RigidbodyMovement : MonoBehaviour
 {
