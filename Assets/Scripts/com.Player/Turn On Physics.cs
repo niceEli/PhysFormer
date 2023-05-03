a444ed2b24095169e29ed55ed6b029150b5018e4
Eli <78963198+niceEli@users.noreply.github.com>
2023-05-03 15:14:47 -0400

    Level12

--- Assets/Scripts/com.Player/Turn On Physics.cs
+++ Assets/Scripts/com.Player/Turn On Physics.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 public class TurnOnPhysics : MonoBehaviour
 {
     [HideInInspector] public Rigidbody2D rb;
+    public float tta;
+    int tagged;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponentInParent<Rigidbody2D>();
     }
 
+
+
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        tagged = other.gameObject.layer;
+        StartCoroutine(SCR());
+    }
+
+    IEnumerator SCR()
+    {
+        if (tagged == LayerMask.NameToLayer("Player"))
         {
-            rb.bodyType = RigidbodyType2D.Dynamic;
+            if (tta == 0f || tta.ToString("") == "")
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                yield return new WaitForEndOfFrame();
+            }
+            else
+            {
+                yield return new WaitForSecondsRealtime(tta);
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            }
         }
     }
 }
