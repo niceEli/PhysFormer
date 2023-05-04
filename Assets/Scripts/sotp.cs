84f4b5304c0fd7d8624e55c5b26c4e8b9791d1d0
Eli <78963198+niceEli@users.noreply.github.com>
2023-05-04 14:41:03 -0400

    HOOD CLASSIC

--- Assets/Scripts/sotp.cs
+++ Assets/Scripts/sotp.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class sotp : MonoBehaviour
 {
     // Start is called before the first frame update
     public void Sotp()
     {
-        Application.Quit();
+        SceneManager.LoadScene("Main Menu");
     }
 }
