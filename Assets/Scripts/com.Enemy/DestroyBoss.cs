a9d32a08e6874c747403f6f699155185387a2b21
Eli <78963198+niceEli@users.noreply.github.com>
2023-05-05 11:57:30 -0400

    HWBHWDBHWBHHBDWHBBHWDBDWHWDHBDW

--- Assets/Scripts/com.Enemy/DestroyBoss.cs
+++ Assets/Scripts/com.Enemy/DestroyBoss.cs
@@ -12,18 +12,23 @@ public class DestroyBoss : MonoBehaviour
     public int health = 3;
     public string credits;
     public TextMeshPro tmp;
+    int timer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the colliding object is on the player layer
-        if (((1 << collision.gameObject.layer) & playerLayer) != 0)
+        if (timer <= 0)
         {
-            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
-            RigidbodyMovement playerM = collision.GetComponent<RigidbodyMovement>();
-            playerRb.velocity = new Vector2(playerRb.velocity.x, playerM.jumpForce);
-            playerM.Grounded = 1;
-            // Destroy the object that this script is attached to
-            health--;
+            // Check if the colliding object is on the player layer
+            if (((1 << collision.gameObject.layer) & playerLayer) != 0)
+            {
+                Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+                RigidbodyMovement playerM = collision.GetComponent<RigidbodyMovement>();
+                playerRb.velocity = new Vector2(playerRb.velocity.x, playerM.jumpForce);
+                playerM.Grounded = 1;
+                // Destroy the object that this script is attached to
+                health--;
+                timer = 25;
+            }
         }
     }
     private void Update()
@@ -33,5 +38,9 @@ public class DestroyBoss : MonoBehaviour
             SceneManager.LoadScene(credits);
         }
         tmp.text = health.ToString("");
+    }
+    private void FixedUpdate()
+    {
+        timer--;
     }
 }
